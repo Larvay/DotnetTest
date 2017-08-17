c556d1a261545fb692bcc77dd954c6cdf540198a
Larvay <larvay@126.com>
2017-08-17 19:00:59 +0800

    My first publishment

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+            System.Console.WriteLine(dt.ToShortDateString());
+        }
+    }
+}
