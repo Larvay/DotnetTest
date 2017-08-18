d2560dd8a6ae3a4cf1e6578129b74ffc9d5b9f59
Larvay <larvay@126.com>
2017-08-18 16:03:22 +0800

    first commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace helloworld
 {
@@ -6,8 +7,13 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            DateTime dt = DateTime.Now;
-            System.Console.WriteLine(dt.ToShortDateString());
+            DateTime dt = DateTime.Today;
+            int correctNumber = new Random().Next(300) + 1;
+            Console.WriteLine($"Now the date is {dt.ToString()}, the random is {correctNumber}");
+
+            var colors = new List<string>() { "blue", "green", "yellow" };
+            colors.ForEach(Console.WriteLine);
+            Console.WriteLine(String.Join(",", colors));
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# DotnetTest" 
+# DotnetTest
